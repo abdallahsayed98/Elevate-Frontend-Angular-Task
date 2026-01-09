d27dce5464d758dd8c237c26a7917dae09cf3efa
Abdallah Sayed <abdallahsayed197@gmail.com>
2026-01-09 16:09:48 +0200

    add initial issue-list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { IssuesListComponent } from './issues-list/issues-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'issues',
+    component: IssuesListComponent,
+    title: 'Home Page'
+  },
+  {
+    path: '**',
+    redirectTo: 'issues'
+  }
+];
---
+++ src/app/issues-list/issues-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-issues-list',
+  imports: [],
+  templateUrl: './issues-list.component.html',
+  styleUrl: './issues-list.component.scss'
+})
+export class IssuesListComponent implements OnInit {
+  posts = signal<Post[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscribition =this.httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/posts').subscribe({
+      next: (resData) =>  {
+        this.posts.set(resData.slice(0,50))
+        console.log(this.posts())
+      },
+      error: (err)=> {
+        this.error.set(err.message);
+      },
+      complete: ()=>{
+        this.isFetching.set(false);
+      }
+    })
+    this.destroyRef.onDestroy(()=>{
+      subscribition.unsubscribe();
+    })
+  }
+}
